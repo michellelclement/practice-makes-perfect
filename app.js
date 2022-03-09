9905cefceadd2b0b8b78ec6f81180f85c7e6d93f
Michelle Clement <michellelclement@gmail.com>
2022-03-09 13:39:06 +0000

    Change inner HTML

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-const title = document.querySelector('h1');
-
-
-//console.log(title.innerText);
-title.innerText = "Hi there!";
-
-
-
-const paragraphs = document.querySelectorAll('p');
-
-paragraphs.forEach(paragraphs => {
-    console.log(paragraphs.innerText);
-    paragraphs.innerText = "This is new text!";
-});
+const errorMsg = document.querySelector(".error-msg");
+errorMsg.innerHTML = '<h2>NewH2</h2>';
