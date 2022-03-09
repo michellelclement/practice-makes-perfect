892c3b3edde29eb3ef66b795ea6677d2cc11a24a
Michelle Clement <michellelclement@gmail.com>
2022-03-09 13:34:20 +0000

    Change all text in multiple same name tags

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const title = document.querySelector('h1');
-const error = document.querySelector('.error');
+
 
 //console.log(title.innerText);
 title.innerText = "Hi there!";
-error.innerText += " or is it?";
+
+
+
+const paragraphs = document.querySelectorAll('p');
+
+paragraphs.forEach(paragraphs => {
+    console.log(paragraphs.innerText);
+    paragraphs.innerText = "This is new text!";
+});
