c8bb6a1398c50140419e1ddd990fddd3eb657be3
Michelle Clement <michellelclement@gmail.com>
2022-03-09 14:32:41 +0000

    Change style by adding and removing classes

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
-const link = document.querySelector("a");
+const content = document.querySelector(".error-text");
+console.log(content.classList);
 
-console.log(link.getAttribute("href"));
-link.setAttribute('href', 'https//www.faceook.com');
-link.innerText = "Facebook baby";
-
-const errorMsg = document.querySelector('.new-error');
-
-
-// Set attribute to an element that doesnt exist
-errorMsg.setAttribute('style', 'color: purple');
-
-//Shows CSS styling
-console.log(errorMsg.style);
-console.log(errorMsg.style.color);
-
-
-errorMsg.style.margin = '150px';
-console.log(errorMsg.style.margin);
-
-errorMsg.style.fontSize = "60px";
-//Empty string reverts back to original setting"
-errorMsg.style.fontSize = "";
+content.classList.add("error-style");
+content.classList.remove("error-style");
+content.classList.add("success-style");
