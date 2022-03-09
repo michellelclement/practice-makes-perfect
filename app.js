c7c7acaea3b6d4f84e39d15259e69eada72cdb74
Michelle Clement <michellelclement@gmail.com>
2022-03-09 14:10:43 +0000

    Change class name by attribute

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const link = document.querySelector("a");
 
 console.log(link.getAttribute("href"));
 link.setAttribute('href', 'https//www.faceook.com');
-link.innerText= "Facebook baby";
+link.innerText = "Facebook baby";
+
+const errorMsg = document.querySelector('.new-error');
+console.log(errorMsg.getAttribute('class'));
+errorMsg.setAttribute('class', 'new-error-message');
